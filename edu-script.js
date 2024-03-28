0d85e80fba305273725e38d4c69abbe9c9b4989e
Benjamin Wild <benwild771@gmail.com>
2024-03-28 09:52:27 -0400

    Starting to add button functionality for education page. Need to work on displaying button information below the class box, instead of below it.

---
+++ edu-script.js
@@ -0,0 +1,26 @@
+// Active elements for Education portion of Website
+
+$(document).ready(async function () {
+  show_content();
+});
+
+async function show(){
+    
+}
+
+async function show_content(){
+    var tags = document.getElementsByClassName("course")
+    var i = 0;
+
+    for(i = 0; i < tags.length; i++){
+        tags[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        } ); 
+    }
+}
